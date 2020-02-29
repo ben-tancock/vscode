4c322ff992d2f73acffbc139c034c4ea669f031a
ben-tancock <ben.tancock@gmail.com>
2020-02-28 16:56:01 -0800

    oops

--- test.js
+++ test.js
@@ -6,5 +6,3 @@ http.createServer();
 
 //If you change something, and then click on the file twice in vscode, you get a diff. Neat!
 // you can use the icon in the bottom left to create a new branch. also neat!
-
-// this line is only in the test branch, not the master branch. see for yourself.
