060f8ec5b3204175125a4034fb6e13a95f310b10
ben-tancock <ben.tancock@gmail.com>
2020-02-28 17:05:49 -0800

    confirming this worked lol

--- test.js
+++ test.js
@@ -4,5 +4,13 @@ console.log(msg);
 var http = require('http');
 http.createServer();
 
-//If you change something, and then click on the file twice in vscode, you get a diff. Neat!
-// you can use the icon in the bottom left to create a new branch. also neat!
+/*  If you change something, and then click on the file twice in vscode, you get a diff. Neat!
+    you can use the icon in the bottom left to create a new branch. also neat!
+    In order to be able to push vscode to github, you need to configure it to a remote repository via the terminal (good ol fashoned way)
+
+    git remote add origin https://github.com/user/repo.git
+
+    git remote -v
+
+    It worked, sweet.
+*/
