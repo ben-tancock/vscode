f86520657f32a707d283fd6b153866788856ba47
ben-tancock <ben.tancock@gmail.com>
2020-02-28 16:55:31 -0800

    this is the master, different from the test

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ console.log(msg);
 
 var http = require('http');
 http.createServer();
+
+//If you change something, and then click on the file twice in vscode, you get a diff. Neat!
+// you can use the icon in the bottom left to create a new branch. also neat!
+
+// this line is only in the test branch, not the master branch. see for yourself.
